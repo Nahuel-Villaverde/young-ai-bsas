669188a01497ab84014c4b050d8e6fcf2ba3f0c7
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2025-03-30 23:47:12 -0300

    cambio astro confg

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://buenosaireshubia.com/', // URL de tu sitio
+  site: 'https://astro-project-gray.vercel.app/', // URL de tu sitio
   integrations: [sitemap(), react()],
 });
